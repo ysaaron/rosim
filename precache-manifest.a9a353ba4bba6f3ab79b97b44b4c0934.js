1ee1040cb9f875b17e5ac303820df5807b62b40a
jigsawye <jigsaw.ye@gmail.com>
2019-07-08 15:18:25 +0800

    auto commit [ci skip]

---
+++ precache-manifest.a9a353ba4bba6f3ab79b97b44b4c0934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c08b6a11f8f966bdb97e8290dbe4b97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248b1ba69d6a0292ae6f",
+    "url": "/static/css/2.6fa1a623.chunk.css"
+  },
+  {
+    "revision": "05579aa7671080585857",
+    "url": "/static/css/main.88a27427.chunk.css"
+  },
+  {
+    "revision": "248b1ba69d6a0292ae6f",
+    "url": "/static/js/2.a1257442.chunk.js"
+  },
+  {
+    "revision": "05579aa7671080585857",
+    "url": "/static/js/main.6841d318.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
